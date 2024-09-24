21f3c0b9cedf7658adb71a278225836f8f2054b5
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-09-24 15:21:18 +0600

    added toggle button colors using dynamically

--- features.js
+++ features.js
@@ -1,9 +1,20 @@
 document.getElementById("donation").addEventListener("click", function () {
+
+  document.getElementById("donation").classList.remove("active");
+  document.getElementById("donation-history").classList.remove("active");
+
+  
+  this.classList.add("active");
+
   showSectionById("donate-cards");
 });
 document
   .getElementById("donation-history")
   .addEventListener("click", function () {
-    console.log("donation");
+
+    document.getElementById("donation").classList.remove("active");
+    document.getElementById("donation-history").classList.remove("active");
+
+    this.classList.add("active");
     showSectionById("donate-history-cards");
   });
